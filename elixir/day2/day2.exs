e9c876b720df44bdb2cf7797fbf727427aac8114
daic0r <daic0r.dev@gmail.com>
2025-12-02 19:37:45 +0100

    Day 2 Elixir

--- elixir/day2/day2.exs
+++ elixir/day2/day2.exs
@@ -1,49 +1,9 @@
 defmodule Aoc2025.Day2 do
 
-  def has_only_repeating_pattern(str_id, cur_pat \\ [], cur_comparison \\ [], in_pattern \\ false)
-  def has_only_repeating_pattern("", cur_pat, cur_pat, _in_pattern) do
-    true
-  end
-  def has_only_repeating_pattern("", cur_pat, cur_comp, _in_pattern) do
-    false
-  end
-  def has_only_repeating_pattern(str_id, cur_pat, cur_comparison, in_pattern) do
-    IO.inspect(cur_pat, label: "cur_pat")
-    IO.inspect(cur_comparison, label: "cur_comparison")
-
-    head = String.at(str_id, 0)
-    tail = String.slice(str_id, 1..String.length(str_id))
-    new_comparison = cur_comparison ++ [head]
-    starts_with = List.starts_with?(cur_pat, new_comparison)
-    if starts_with do
-      has_only_repeating_pattern(tail, cur_pat, new_comparison, true) 
-    else 
-      if not in_pattern do
-        IO.inspect(new_comparison, label: "THIS")
-        has_only_repeating_pattern(tail, cur_pat ++ [head], [])
-      else
-        has_only_repeating_pattern(tail, cur_pat ++ [head], [head], true)
-      end
-    end
-  end
-
-
-  def check_range_for_invalid_ids(min_id, max_id, cur_id, out_invalid \\ [])
-  def check_range_for_invalid_ids(min_id, max_id, cur_id, out_invalid) when cur_id == max_id + 1 do
-    out_invalid
-  end
-  def check_range_for_invalid_ids(min_id, max_id, cur_id, out_invalid) do
-    res =
-      if has_only_repeating_pattern(Integer.to_string(cur_id)) do
-        [cur_id | out_invalid]
-      else
-        out_invalid
-      end
-    check_range_for_invalid_ids(min_id, max_id, cur_id + 1, res)
-  end
-
+#################################################
+# For Part 1
   def check_range_simple(min_id, max_id, cur_id, out_invalid \\ [])
-  def check_range_simple(min_id, max_id, cur_id, out_invalid) when cur_id == max_id + 1 do
+  def check_range_simple(_min_id, max_id, cur_id, out_invalid) when cur_id == max_id + 1 do
     out_invalid
   end
   def check_range_simple(min_id, max_id, cur_id, out_invalid) do
@@ -59,6 +19,7 @@ defmodule Aoc2025.Day2 do
       check_range_simple(min_id, max_id, cur_id + 1, out_invalid)
     end
   end
+#################################################
 
 
   def part1(input) do
@@ -66,6 +27,7 @@ defmodule Aoc2025.Day2 do
     |> Enum.filter(fn {min, max} ->
       Integer.mod(String.length(min), 2) == 0 or Integer.mod(String.length(max), 2) == 0
     end)
+# Remove items that are definitely not invalid
     |> Enum.map(fn {min, max} ->
       min_int = String.to_integer(min)
       max_int = String.to_integer(max)
@@ -84,12 +46,55 @@ defmodule Aoc2025.Day2 do
         end
       {lower_bound, upper_bound}
     end)
-    |> IO.inspect
     |> Enum.flat_map(fn {min_id, max_id} ->
       check_range_simple(min_id, max_id, min_id)
     end)
     |> Enum.sum
   end
+
+  
+#################################################
+# For Part 2
+  def split_string_equal_length_parts(str, len, out \\ [])
+  def split_string_equal_length_parts("", _len, out) do
+    out 
+  end
+  def split_string_equal_length_parts(str, len, out) do
+    split_string_equal_length_parts(String.slice(str, len..String.length(str)), len, [String.slice(str, 0..len-1) | out])
+  end
+
+  def has_only_repeating_pattern(str_id, sequence_len \\ 1)
+  def has_only_repeating_pattern(str_id, sequence_len) do
+    if sequence_len > div(String.length(str_id), 2) do
+      false
+    else
+      if Integer.mod(String.length(str_id), sequence_len) == 0 do
+        parts = split_string_equal_length_parts(str_id, sequence_len)
+        if not Enum.all?(parts, & &1 == hd(parts)) do
+          has_only_repeating_pattern(str_id, sequence_len + 1)
+        else
+          true
+        end
+      else
+        has_only_repeating_pattern(str_id, sequence_len + 1)
+      end
+    end
+  end
+
+  def check_range_for_invalid_ids(min_id, max_id, cur_id, out_invalid \\ [])
+  def check_range_for_invalid_ids(_min_id, max_id, cur_id, out_invalid) when cur_id == max_id + 1 do
+    out_invalid
+  end
+  def check_range_for_invalid_ids(min_id, max_id, cur_id, out_invalid) do
+    res =
+      if has_only_repeating_pattern(Integer.to_string(cur_id)) do
+        [cur_id | out_invalid]
+      else
+        out_invalid
+      end
+    check_range_for_invalid_ids(min_id, max_id, cur_id + 1, res)
+  end
+#################################################
 
   def part2(input) do
     input
@@ -99,15 +104,14 @@ defmodule Aoc2025.Day2 do
     |> Enum.flat_map(fn {min, max} ->
       check_range_for_invalid_ids(min, max, min) 
     end)
-    |> IO.inspect
     |> Enum.sum
   end
-  
+
 end
 
 
-input="11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-#input = File.read!("input.txt")
+#input="11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+input = File.read!("input.txt")
   |> String.trim_trailing("\n")
   |> String.split(",")
   |> Enum.map(fn rng ->
