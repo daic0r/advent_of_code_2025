2e4f039826e93e730e658c5b0964d8c9e81436e6
daic0r <daic0r.dev@gmail.com>
2025-12-08 16:49:50 +0100

    Day 8 Elixir Part 2

--- elixir/day8/day8.exs
+++ elixir/day8/day8.exs
@@ -41,7 +41,11 @@ defmodule Aoc2025.Day8 do
       d = dist(head, vec)
       :orddict.store(d, {head, vec}, stor)
     end)
-    gen_distances2(tail, limit, new_out |> Enum.take(limit))
+    new_out = case limit do
+      :infinity -> new_out
+      n -> new_out |> Enum.take(limit)
+    end
+    gen_distances2(tail, limit, new_out)
   end
 
   def get_distance(distances, a, b) do
@@ -65,10 +69,6 @@ defmodule Aoc2025.Day8 do
     {{vec_a, vec_b}, distances} = :orddict.take(elem(hd(distances), 0), distances)
     circ_a = Enum.find(out_circuits, fn circuit -> vec_a in circuit and vec_b not in circuit end)
     circ_b = Enum.find(out_circuits, fn circuit -> vec_b in circuit and vec_a not in circuit end)
-
-    if (circ_a != nil and circ_b == nil) or (circ_a == nil and circ_b != nil) do
-      IO.puts "PROCEED WITH CAUTION"
-    end
 
     # Merge the two circuits if the two points aren't already part of the same
     # circuit
@@ -110,31 +110,63 @@ defmodule Aoc2025.Day8 do
   end
 
   
+  def part2(distances, out_circuits)
+  # def part2(distances, out_circuits) when length(out_circuits) == 1 do
+  #   out_circuits
+  #     |> Enum.sort_by(& length(&1), :desc)
+  #     |> Enum.take(3)
+  #     |> IO.inspect(limit: :infinity)
+  #     |> Enum.product_by(& length(&1))
+  # end
+  def part2(distances, out_circuits) do
+    {{vec_a, vec_b}, distances} = :orddict.take(elem(hd(distances), 0), distances)
+    circ_a = Enum.find(out_circuits, fn circuit -> vec_a in circuit and vec_b not in circuit end)
+    circ_b = Enum.find(out_circuits, fn circuit -> vec_b in circuit and vec_a not in circuit end)
+
+    # Merge the two circuits if the two points aren't already part of the same
+    # circuit
+    out_circuits = if circ_a != nil and circ_b != nil do
+      merged = circ_a ++ circ_b
+
+      out_circuits = out_circuits |> Enum.reject(& &1 == circ_b or &1 == circ_a)
+
+      out_circuits = [merged | out_circuits]
+
+      if length(out_circuits) == 1 do
+        vec_a.x * vec_b.x
+      else
+        part2(distances, out_circuits)
+      end
+    else
+        part2(distances, out_circuits)
+    end
+
+  end
 end
 
-input = "162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689"
+# input = "162,817,812
+# 57,618,57
+# 906,360,560
+# 592,479,940
+# 352,342,300
+# 466,668,158
+# 542,29,236
+# 431,825,988
+# 739,650,466
+# 52,470,668
+# 216,146,977
+# 819,987,18
+# 117,168,530
+# 805,96,715
+# 346,949,466
+# 970,615,88
+# 941,993,340
+# 862,61,35
+# 984,92,344
+# 425,690,689"
 #
 #
-# input = File.read!("input.txt")
+input = File.read!("input.txt")
 
 input = input
   |> String.split("\n")
@@ -145,8 +177,11 @@ input = input
 circuits = input
   |> Enum.map(fn vec -> [vec] end)
 
-distances = Aoc2025.Day8.gen_distances2(input, 10)
+distances = Aoc2025.Day8.gen_distances2(input, 1000) |> IO.inspect(limit: :infinity)
 
-IO.puts("Launching")
-sol1 = Aoc2025.Day8.part1(distances, circuits, 10)
+sol1 = Aoc2025.Day8.part1(distances, circuits, 1000)
 IO.puts "Part 1: #{sol1}"
+
+distances = Aoc2025.Day8.gen_distances2(input, 8000)
+sol2 = Aoc2025.Day8.part2(distances, circuits)
+IO.puts "Part 2: #{sol2}"
