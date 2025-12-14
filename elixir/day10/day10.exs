9229951a7758fab7d44df9f867e02e59372fe04f
daic0r <daic0r.dev@gmail.com>
2025-12-14 11:43:08 +0100

    Day 10 Elixir Part 1

--- elixir/day10/day10.exs
+++ elixir/day10/day10.exs
@@ -1,7 +1,7 @@
 defmodule Aoc2025.Day10.Machine do
   use Bitwise
 
-  defstruct [:lights, :num_lights, :target_lights, :buttons, :joltages]
+  defstruct [:lights, :num_lights, :target_lights, :buttons, :num_buttons, :joltages]
 
   def lights_from_string(str) do
     num_lights = String.length(str) - 2
@@ -37,43 +37,24 @@ defmodule Aoc2025.Day10.Machine do
     # [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
     rex = ~r/(\[[\.#]+\]) ((?:\((?:\d+,?)+\) ?)+)(\{(?:\d+,?)+\})/
     [[lights, buttons, joltages]]  = Regex.scan(rex, str, capture: :all_but_first) 
+    buttons = buttons_from_string(buttons |> String.trim_trailing, String.length(lights)-2)
     %__MODULE__{
       lights: 0,
       num_lights: String.length(lights)-2,
       target_lights: lights_from_string(lights),
-      buttons: buttons_from_string(buttons |> String.trim_trailing, String.length(lights)-2)
+      buttons: buttons,
+      num_buttons: length(buttons)
     }
   end
 
   def press_button(%__MODULE__{ buttons: buttons, lights: lights, num_lights: num_lights } = machine, idx) do
     button = Enum.at(buttons, idx)
     new_lights = bxor(lights, button)
-    # new_lights = Enum.reduce(0..num_lights-1, lights, fn bit_idx, lights_acc ->
-    #   bit_pos = num_lights - bit_idx - 1
-    #   if (button &&& (1 <<< bit_pos)) == (1 <<< bit_pos) do
-    #     # Bit in lights is set, build mask to AND with
-    #     if (lights &&& (1 <<< bit_pos)) == (1 <<< bit_pos) do
-    #       mask = Enum.reduce(0..num_lights-1, 0, fn idx, acc ->
-    #         acc + if idx == bit_idx, do: 0, else: (1 <<< (num_lights - idx - 1))
-    #       end)
-    #       lights_acc &&& mask
-    #     else
-    #     # Bit in lights is not set, build mask to OR with
-    #       mask = Enum.reduce(0..num_lights-1, 0, fn idx, acc ->
-    #         acc + if idx == bit_idx, do: (1 <<< (num_lights - idx - 1)), else: 0
-    #       end)
-    #       lights_acc ||| mask
-    #     end
-    #   else
-    #     lights_acc
-    #   end
-    # end)
 
     %__MODULE__{ machine | lights: new_lights }
   end
 
   def started?(%__MODULE__{ lights: lights, num_lights: num_lights, target_lights: target_lights }) do
-    #(1 <<< num_lights) == lights + 1 
     lights == target_lights
   end
 end
@@ -81,17 +62,29 @@ end
 defmodule Aoc2025.Day10 do
   alias Aoc2025.Day10.Machine
 
+  # Each button press will result in a different machine state, therefore after
+  # each iteration of N buttons we will also have N new machine states to
+  # continue with for each button (BFS).
   def find_sequence(machines, seq_len \\ 1) do
-    {found, next_machines} = Enum.reduce_while(machines, {false, []} , fn machine, {_, next_machines} -> 
-      new_machines = Enum.map(0..length(machine.buttons)-1, fn btn_idx ->
-        Machine.press_button(machine, btn_idx) 
+    # Each machine produces a list of new machine states after each individual
+    # button press
+    {found?, next_machines} = Enum.reduce_while(machines, {false, []} , fn machine, {_, next_machines} ->
+      # For each machine, every button can be pressed again
+      {found?, next_machines_for_button} = Enum.reduce_while(0..machine.num_buttons-1, {false, []}, fn btn_idx, {_, next_machines} ->
+        next_machine = Machine.press_button(machine, btn_idx)
+        case Machine.started?(next_machine) do
+          true -> {:halt, {true, [next_machine | next_machines]}}
+          false -> {:cont, {false, [next_machine | next_machines]}}
+        end
       end)
-      case Enum.any?(new_machines, &Machine.started?/1) do
+      case found? do
         true -> {:halt, {true, []}}
-        false -> {:cont, {false, [new_machines | next_machines]}}
+        false -> {:cont, {false, [next_machines_for_button | next_machines]}}
       end
     end)
-    if found do
+    # Here we have a list of lists of machines, therefore we flatten it down
+    # below
+    if found? do
       seq_len
     else
       find_sequence(next_machines |> List.flatten, seq_len + 1)
@@ -102,7 +95,7 @@ defmodule Aoc2025.Day10 do
       |> Enum.map(fn machine ->
         find_sequence([machine]) 
       end)
-      |> IO.inspect
+      |> IO.inspect(limit: :infinity)
       |> Enum.sum()
   end
 end
